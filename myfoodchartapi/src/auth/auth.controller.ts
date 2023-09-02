659c771097d6cd3583defeee8ee92fb9ff243861
RunningLearner <tkacnsdms@naver.com>
2023-09-02 16:04:16 +0900

    feat: 소셜로그인 기능 추가

---
+++ myfoodchartapi/src/auth/auth.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, UseGuards, Req, Res } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { Response } from 'express';
+/* Google Strategy */
+type GoogleUser = {
+  email: string;
+  firstName: string;
+  lastName: string;
+  photo: string;
+};
+export type GoogleRequest = Request & { user: GoogleUser };
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleLogin(@Req() req: GoogleRequest, @Res() res: Response) {
+    this.authService.OAuthLogin(req, res);
+    // Google 로그인 페이지로 리다이렉트 됩니다.
+  }
+
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  googleLoginCallback(@Req() req) {
+    // 구글 로그인 성공 후 실행되는 코드
+    return {
+      message: 'Google 로그인 성공',
+      user: req.user,
+    };
+  }
+}
---
+++ myfoodchartapi/src/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, Res } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { GoogleRequest } from './auth.controller';
+import { Response } from 'express';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async OAuthLogin(req: GoogleRequest, @Res() res: Response) {
+    const user = req.user; // Google에서 전달받은 사용자 정보
+
+    // 1. 회원조회
+    // let user = await this.usersService.findOne({ email: req.user.email }); //user를 찾아서
+
+    // 2, 회원가입이 안되어있다면? 자동회원가입
+    //if (!user) user = await this.usersService.create({ ...req.user }); //user가 없으면 하나 만들고, 있으면 이 if문에 들어오지 않을거기때문에 이러나 저러나 user는 존재하는게 됨.
+
+    // 3. 회원가입이 되어있다면? 로그인(AT, RT를 생성해서 브라우저에 전송)한다
+
+    // JWT 토큰 생성
+    const payload = { username: user.firstName, email: user.email };
+    const accessToken = this.jwtService.sign(payload);
+
+    res.cookie('access_token', accessToken); // 이제 .cookie 메서드를 사용할 수 있습니다.
+
+    return res.json({
+      message: 'Google 로그인 성공',
+      user: req.user,
+    });
+  }
+}
