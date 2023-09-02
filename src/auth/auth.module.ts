e829ee5f65b7baeb11864f27c6732d28d9a65d4e
RunningLearner <tkacnsdms@naver.com>
2023-09-02 16:36:36 +0900

    refactor: 루트폴더로 변경

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { GoogleStrategy } from './strategies/google.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [PassportModule.register({ defaultStrategy: 'google' })],
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategy],
+  providers: [AuthService, GoogleStrategy, JwtService],
 })
 export class AuthModule {}
