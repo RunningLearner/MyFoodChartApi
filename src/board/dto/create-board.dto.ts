65ce02f52be9fbe6fc7d2deb545e34c1b58bcec2
남승인 <sinam@sds.co.kr>
2023-10-06 08:08:32 +0900

    faet: createDto 추가

--- src/board/dto/create-board.dto.ts
+++ src/board/dto/create-board.dto.ts
@@ -1,1 +1,39 @@
-export class CreateBoardDto {}
+import { IsString, IsNumber, IsBoolean } from 'class-validator';
+
+export class CreateBoardDto {
+  @IsString()
+  nickname: string;
+
+  @IsString()
+  institute: string;
+
+  @IsNumber()
+  peopleNum: number;
+
+  @IsNumber()
+  price: number;
+
+  @IsString()
+  imgUrl: string;
+
+  @IsString()
+  menuName: string;
+
+  @IsBoolean()
+  isProductUsed: boolean;
+
+  @IsString()
+  productName: string;
+
+  @IsString()
+  productBrand: string;
+
+  @IsString()
+  explanation: string;
+
+  @IsString()
+  fileURL: string;
+
+  @IsString()
+  boardType: string;
+}
