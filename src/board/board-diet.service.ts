ef102141090821493aa71f6b59110360f6047425
남승인 <sinam@sds.co.kr>
2023-10-10 08:00:32 +0900

    feat: create 기능 초안

--- src/board/board-diet.service.ts
+++ src/board/board-diet.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './entities/board.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class BoardDietService {
-  create(createBoardDto: CreateBoardDto) {
-    return 'This action adds a new board';
+  constructor(
+    @InjectRepository(Board)
+    private boardsRepository: Repository<Board>,
+  ) {}
+
+  async create(createBoardDto: CreateBoardDto) {
+    const newPost = this.boardsRepository.create(createBoardDto);
+    return await this.boardsRepository.save(newPost);
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all board`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} board`;
   }
 
-  update(id: number, updateBoardDto: UpdateBoardDto) {
+  async update(id: number, updateBoardDto: UpdateBoardDto) {
     return `This action updates a #${id} board`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} board`;
   }
 }
