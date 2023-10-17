dc8860559470793928fb4771ed07eebd11d7a404
RunningLearner <tkacnsdms@naver.com>
2023-10-18 07:46:45 +0900

    feat: 부트 스트랩 로거 winston으로 대체

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true, // 부트스트래핑 과정까지 nest-winston 로거 사용
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
   const port = process.env.PORT || 3001;
+
   app.enableCors({
     origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+
   await app.listen(port);
-  console.log(`Server running on http://localhost:${port}`);
 }
 bootstrap();
