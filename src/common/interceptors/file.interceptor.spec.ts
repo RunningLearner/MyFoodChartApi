c845c1d73948279397720b762bb1e1fd6b32dd6b
RunningLearner <tkacnsdms@naver.com>
2023-10-17 20:57:33 +0900

    feat: 파일 인터셉터 테스트 코드 추가

---
+++ src/common/interceptors/file.interceptor.spec.ts
@@ -0,0 +1,57 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ExecutionContext, CallHandler } from '@nestjs/common';
+import { FileInterceptor } from './file.interceptor';
+import { of } from 'rxjs';
+import * as path from 'path';
+import * as fs from 'fs';
+
+describe('FileInterceptor', () => {
+  let interceptor: FileInterceptor;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [FileInterceptor],
+    }).compile();
+
+    interceptor = module.get<FileInterceptor>(FileInterceptor);
+  });
+
+  it('should handle file upload and attach URLs', async () => {
+    // 1. 테스트용 요청 객체 생성
+    const mockReq = {
+      files: {
+        imgfile: [
+          { originalname: 'testImage.jpeg', filename: 'testImage_saved.jpeg' },
+        ],
+      },
+      body: { imgfileUrl: '' },
+    };
+
+    const mockExecutionContext = {
+      switchToHttp: () => ({
+        getRequest: () => mockReq,
+      }),
+    } as unknown as ExecutionContext;
+
+    const mockCallHandler = {
+      handle: () => of('test'),
+    } as unknown as CallHandler;
+
+    // 2. 인터셉터 실행
+    await new Promise<void>((resolve) => {
+      interceptor.intercept(mockExecutionContext, mockCallHandler).subscribe({
+        complete: () => resolve(),
+      });
+    });
+
+    // 3. 결과 검증
+    expect(mockReq.body.imgfileUrl).toBeDefined();
+    const filePath = path.resolve(__dirname, '..', mockReq.body.imgfileUrl);
+    expect(fs.existsSync(filePath)).toBe(true);
+
+    // 테스트가 끝나면 파일을 삭제
+    // if (fs.existsSync(filePath)) {
+    //   fs.unlinkSync(filePath);
+    // }
+  });
+});
