06c6d6909343e77d73a9f99fdd373b974610368a
RunningLearner <tkacnsdms@naver.com>
2023-10-25 22:22:59 +0900

    feat: update test 추가

--- test/board.e2e-spec.ts
+++ test/board.e2e-spec.ts
@@ -8,7 +8,8 @@ import * as jwt from 'jsonwebtoken';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-  let fakeToken: string;
+  const user = { email: 'test@test.com' };
+  const fakeToken: string = jwt.sign(user, process.env.JWT_SECRET);
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -22,8 +23,6 @@ describe('AppController (e2e)', () => {
   it('게시글 생성', () => {
     const filePath1 = path.resolve(__dirname, './test-assets/testImage.jpeg');
     const filePath2 = path.resolve(__dirname, './test-assets/testFile.pdf');
-    const user = { email: 'test@test.com' };
-    fakeToken = jwt.sign(user, process.env.JWT_SECRET);
 
     const menues = [
       {
@@ -52,6 +51,49 @@ describe('AppController (e2e)', () => {
       .then((response) => {
         expect(response.body).toBeDefined();
         // 여기에 추가적으로 응답 데이터에 대한 검증 로직을 넣을 수 있습니다.
+      });
+  });
+
+  it('게시글 업데이트', () => {
+    const updateData = {
+      date: '2021-12-13',
+      institute: 'Updated Institute',
+      peopleNum: '6',
+      price: '12000',
+      explanation: 'This is an updated test',
+      whichSchool: 'Updated School',
+      menues: [
+        {
+          id: 1, // Assuming this menu item exists
+          menuName: 'Updated Menu',
+          isProductUsed: false,
+          productName: 'Updated Product',
+          productBrand: 'Updated Brand',
+        },
+        // ... other updated menu items
+      ],
+    };
+
+    // Assuming the post to be updated has ID 1
+    const postId = 1;
+
+    return request(app.getHttpServer())
+      .patch(`/boards/diet/${postId}`) // Assuming the update endpoint is PATCH /boards/diet/:id
+      .set('authorization', `Bearer ${fakeToken}`)
+      .field('date', updateData.date)
+      .field('institute', updateData.institute)
+      .field('peopleNum', updateData.peopleNum)
+      .field('price', updateData.price)
+      .field('explanation', updateData.explanation)
+      .field('whichSchool', updateData.whichSchool)
+      .field('menues', JSON.stringify(updateData.menues))
+      .expect(204) // Assuming a 200 OK response is expected
+      .then((response) => {
+        expect(response.body).toBeDefined();
+        // Additional assertions to verify the updated data
+        expect(response.body.date).toEqual(updateData.date);
+        expect(response.body.institute).toEqual(updateData.institute);
+        // ... other assertions
       });
   });
 });
