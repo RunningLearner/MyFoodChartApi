2a56a5861b18d0da3cb0edd33b439399dd2da3a5
RunningLearner <97277365+RunningLearner@users.noreply.github.com>
2023-12-03 13:41:00 +0900

    temp: tes 시도

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! CICD로 바뀜!';
+    return 'Hello World! 테스트입니다!';
   }
 }
